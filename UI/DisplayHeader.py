2628746f8852cfc250674ac99b0244480119c23a
Arnar Arnarson <arnara17@ru.is>
2018-12-04 18:39:32 +0000

    smávægileg breyting á main menu

--- UI/DisplayHeader.py
+++ UI/DisplayHeader.py
@@ -5,8 +5,13 @@ class DisplayHeader:
 
     def display_header(self):
         os.system('cls')
-        print("\t********* Welcome to Bragginn Car Rental ********* \n"  #Create a seperate function called displayTitlebar
-            "\tblow your horn on a car from Bragginn car rental! \n"
-            "\t************************************************** \n")
-
+        print(  "\t ______                         _                 ______               ______                        _ \n"#Create a seperate function called displayTitlebar
+                "\t(____  \                       (_)               / _____)             (_____ \             _        | |\n"
+                "\t ____)  ) ____ ____  ____  ____ _ ____  ____    | /      ____  ____    _____) ) ____ ____ | |_  ____| |\n"
+                "\t|  __  ( / ___) _  |/ _  |/ _  | |  _ \|  _ \   | |     / _  |/ ___)  (_____ ( / _  )  _ \|  _)/ _  | |\n"
+                "\t| |__)  ) |  ( ( | ( ( | ( ( | | | | | | | | |  | \____( ( | | |            | ( (/ /| | | | |_( ( | | |\n"
+                "\t|______/|_|   \_||_|\_|| |\_|| |_|_| |_|_| |_|   \______)_||_|_|            |_|\____)_| |_|\___)_||_|_|\n"
+                "\t                   (_____(_____|                                                                       \n"
+                "\t************************blow your horn on a car from Bragginn car rental!****************************** \n"
+                )
 #Create submenu headers
