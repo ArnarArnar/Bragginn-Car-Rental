6756d7e7a8b529653ec1a9fa3276c1b04534e407
dagurdagur <dagurdagur@gmail.com>
2018-12-05 08:47:45 +0000

    Get and set functions for Car.py

--- Models/Car.py
+++ Models/Car.py
@@ -17,4 +17,34 @@ class Car:
         return '{}  :   {}  :   {}  :   {}  :   {}'.format(self._car_id, 
                 self._brand, self._year, self._price_per_day, self._car_type)
 
-#Need to add get and set functions for all the elements
+#Get functions
+    def get_car_id(self):
+        return self._car_id
+
+    def get_car_brand(self):
+        return self._brand
+
+    def get_car_year(self):
+        return self._year
+
+    def get_car_price_per_day(self):
+        return self._price_per_day
+
+    def get_car_type(self):
+        return self._car_type
+
+#Set functions
+    def set_car_id(self, car_id):
+        self._car_id = car_id
+
+    def set_car_brand(self, brand):
+        self._brand = brand
+
+    def set_car_year(self, year):
+        self._year = year
+
+    def set_car_price_per_day(self, price_per_day):
+        self._price_per_day = price_per_day
+
+    def set_car_type(self, car_type):
+        self._car_type = car_type
