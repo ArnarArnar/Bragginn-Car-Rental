365cdc7dfb4712788aeb335b27064e132003feba
dagurdagur <34142077+dagurdagur@users.noreply.github.com>
2018-12-05 23:15:35 +0000

    Error fix Rent a car and Add rental

--- UI/CarSubMenu.py
+++ UI/CarSubMenu.py
@@ -44,14 +44,13 @@ class CarSubMenu:
             car_id = input("Enter car ID (AADDD): ")
             self.valid = self._validation_service.is_car_id_valid(car_id)
             if not self.valid:
-                print("Car Id needs to be in the format (AADDD)")
+                print("Car Id can not be longer then X ")
                 os.system('pause')
                 continue
-            self.valid = self._validation_service.does_car_id_exist(car_id)
-            if self.valid:
+            self.valid = not self._validation_service.does_car_id_exist(car_id)
+            if not self.valid:
                 print("Car Id already exists")
                 os.system('pause')
-                self.valid = False
         brand = input("Enter car brand: ")
         self.valid = False
         while not self.valid:
