7e487ac05461b2058a03d151314251e3c833ede9
Karen María <karenmaria@Karens-MacBook-Pro.local>
2018-12-06 13:27:11 +0000

    get og set búin til

--- Models/Customer.py
+++ Models/Customer.py
@@ -24,3 +24,61 @@ class Customer:
                 self._country, self._drivers_license)
 
 #Need to add get and set functions for all the elements
+
+#GetFunctions
+
+    def get_customer_id(self):
+        return self._customer_id
+    
+    def get_first_name(self):
+        return self._first_name
+
+    def get_last_name(self):
+        return self._last_name
+    
+    def get_phone(self):
+        return self._phone
+    
+    def get_street(self):
+        return self._street
+    
+    def get_zip(self):
+        return self._zip
+    
+    def get_town(self):
+        return self._town
+    
+    def get_country(self):
+        return self._country
+    
+    def get_drivers_license(self):
+        return self._drivers_license
+
+#SetFunctions
+
+    def set_customer_id(self, customer_id):
+        self._customer_id = customer_id
+    
+    def set_first_name(self, first_name):
+        self._first_name = first_name
+    
+    def set_last_name(self, last_name):
+        self._last_name = last_name
+    
+    def set_phone(self, phone):
+        self._phone = phone
+    
+    def set_street(self, street):
+        self._street = street
+    
+    def set_zip(self, zip):
+        self._zip = zip
+    
+    def set_town(self, town):
+        self._town = town
+
+    def set_country(self, country):
+        self._country = country
+    
+    def set_drivers_license(self, drivers_license):
+        self._drivers_license = drivers_license
--- Models/Rental.py
+++ Models/Rental.py
@@ -21,3 +21,51 @@ class Rental:
                 
 
 #Need to add get and set functions for all the elements
+
+#GetFunctions
+
+    def get_order_id(self):
+        return self._order_id
+
+    def get_customer_id(self):
+        return self._customer_id
+
+    def get_car_id(self):
+        return self._car_id
+    
+    def get_start_date(self):
+        return self._start_date
+
+    def get_days(self):
+        return self._days
+    
+    def get_insurance(self):
+        return self._insurance
+
+    def get_total_price(self):
+        return self._total_price
+        
+#SetFunctions
+
+    def set_order_id(self, order_id):
+        self._order_id = order_id
+    
+    def set_customer_id(self, customer_id):
+        self._customer_id = customer_id
+    
+    def set_car_id(self, car_id):
+        self._car_id = car_id
+
+    def set_start_date(self, start_date):
+        self._start_date = start_date
+    
+    def set_days(self, days):
+        self._days = days
+    
+    def set_insurance(self, insurance):
+        self._insurance = insurance
+
+    def set_total_price(self, total_price):
+        self._total_price = total_price
+
+    
