cf4d40911e63a68b204280a3499adf9df681cca1
dagurdagur <dagurdagur@gmail.com>
2018-12-11 14:26:49 +0000

    Rental repo fix nr2

--- Repositories/RentalRepository.py
+++ Repositories/RentalRepository.py
@@ -18,23 +18,23 @@ class RentalRepository:
     def add_rental(self, Rental):
         rental_date = datetime.strftime(Rental._start_date, '%d %m %Y')
         rental_end_date = datetime.strftime(Rental._end_date, '%d %m %Y')
-        with open(os.path.realpath('Data/Rentals.csv'), 'a+', newline='', encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Rentals.csv'), 'a+', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
             csv_writer.writerow([Rental._order_id, Rental._customer_id, Rental._car_id,
                                 rental_date, Rental._days, Rental._insurance, Rental._total_price, rental_end_date])
 
     def add_order_id(self, order_id):
-        with open(os.path.realpath('Data/OrderIDs.csv'), 'a+', newline='', encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/OrderIDs.csv'), 'a+', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
             csv_writer.writerow([order_id])
     
     def add_insurance(self, Insurance):
-        with open(os.path.realpath('Data/Insurance.csv'), 'a+', newline='', encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Insurance.csv'), 'a+', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
             csv_writer.writerow([Insurance._short_code, Insurance._name, Insurance._price])
 
     def overwrite_rentals_list(self, rentals_list):
-        with open(os.path.realpath('Data/Rentals.csv'), 'w', newline='', encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Rentals.csv'), 'w', newline='') as csv_file:
                 csv_writer = csv.writer(csv_file, delimiter=';')
                 for rental in rentals_list:
                     csv_writer.writerow([rental._order_id, rental._customer_id, rental._car_id,
@@ -42,7 +42,7 @@ class RentalRepository:
                                 rental._total_price, datetime.strftime(rental._end_date, '%d %m %Y')])
 
     def add_return(self, CarReturn):
-        with open(os.path.realpath('Data/Returns.csv'), 'a+', newline='', encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Returns.csv'), 'a+', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';')
             csv_writer.writerow([CarReturn._order_id, CarReturn._days_late, CarReturn._gas_level, CarReturn._return_comment])
 
@@ -50,7 +50,7 @@ class RentalRepository:
 #Get functions
     def get_rental_list(self):
         self.__Rentals.clear()
-        with open(os.path.realpath('Data/Rentals.csv'), encoding='ASCII') as rentals_file:
+        with open(os.path.realpath('Data/Rentals.csv')) as rentals_file:
             rentals_reader = csv.reader(rentals_file, delimiter=';')
             rentals_list = list(rentals_reader)
             for row in rentals_list:
@@ -63,7 +63,7 @@ class RentalRepository:
     
     def get_insurance_list(self):
         self.__Insurance.clear()
-        with open(os.path.realpath('Data/Insurance.csv'), encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Insurance.csv')) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             insurance_list = list(csv_reader)
             for row in insurance_list:
@@ -73,7 +73,7 @@ class RentalRepository:
 
     def get_returns_list(self):
         Returns = []
-        with open(os.path.realpath('Data/Returns.csv'), encoding='ASCII') as csv_file:
+        with open(os.path.realpath('Data/Returns.csv')) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             returns_list = list(csv_reader)
             for row in returns_list:
@@ -82,7 +82,7 @@ class RentalRepository:
         return Returns
 
     def get_next_order_id(self):
-        with open(os.path.realpath('Data/OrderIDs.csv'), encoding='ASCII') as order_id_file:
+        with open(os.path.realpath('Data/OrderIDs.csv')) as order_id_file:
             order_id_reader = csv.reader(order_id_file, delimiter=';')
             id_list = list(order_id_reader)
             next_id = int(id_list[-1][0]) + 1
@@ -90,7 +90,7 @@ class RentalRepository:
 
     def get_rentals_primary_keys(self):
         self._primary_keys.clear()
-        with open(os.path.realpath('Data/Rentals.csv'), encoding='ASCII') as customer_file:
+        with open(os.path.realpath('Data/Rentals.csv')) as customer_file:
             csv_reader = csv.reader(customer_file, delimiter=';')
             customer_list = list(csv_reader)
             for row in customer_list:
@@ -99,7 +99,7 @@ class RentalRepository:
 
     def get_insurance_primary_keys(self):
         self._primary_keys.clear()
-        with open(os.path.realpath('Data/Insurance.csv'), encoding='ASCII') as customer_file:
+        with open(os.path.realpath('Data/Insurance.csv')) as customer_file:
             csv_reader = csv.reader(customer_file, delimiter=';')
             customer_list = list(csv_reader)
             for row in customer_list:
